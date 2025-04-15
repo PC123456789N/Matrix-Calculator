098f0100b26bb8b427c700a06133bb6f742d95c6
brasilicioh <brasiliciohcr@gmail.com>
2025-04-15 20:56:06 -0300

    alterações

---
+++ Matrix.java
@@ -0,0 +1,11 @@
+// Codigo que calcula automaticamente matrizes em java
+// autores: Brasilicio H e Kaio H
+//     (Os Henriques)
+
+
+public class Matrix {
+    public static void main(String[] args) {
+        // Calculadora de matrix
+        // oi caio
+    }
+}
---
+++ python/matrix.py
@@ -0,0 +1,13 @@
+# calculadora automatica de matrix (sem numpy pq sim)
+# caio, nao mexe no meu codego. se quiser fazer em python, faça outro arquivo
+
+
+
+# Primeiro: entrada de operacao (soma, subtracao e multiplicacao)
+# depois add determinante, trasnposto, cofator, adjunto e inverso
+
+# depois da entrada; funcao para ler ordem e valores das matrizes
+# soma/subtracao: matrizes de mesma ordem. forma nova matriz que valor de c(ij) = a(ij) +- b(ij)
+
+# multiplicacao: coluna da primeira matriz igual linha da segunda matriz. 
+# forma nova matriz de qtd de linha da primeira matriz e qtd coluna da segunda matriz
