7012199e0294d5d644ac8a043286a5cc9344737a
brasilicioh <brasiliciohcr@gmail.com>
2025-04-22 22:22:50 -0300

    inicio de javazin

--- Matrix.java
+++ Matrix.java
@@ -2,10 +2,28 @@
 // autores: Brasilicio H e Kaio H
 //     (Os Henriques)
 
+import java.util.Scanner;
 
 public class Matrix {
+
+    // criar funcao que le os valores de uma matriz aqui
+
     public static void main(String[] args) {
-        // Calculadora de matrix
-        // oi caio
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Escolha sua operação: \n" +
+                                "1. Soma (+) \n" +
+                                "2. Subtracao (-) \n" +
+                                "3. Multiplicacao (*) \n" +
+                                "0. Sair (s)");
+            String operacao = scanner.nextLine();
+
+            System.out.println(operacao);
+
+            scanner.close();
+
+            break;
+        }
     }
 }
