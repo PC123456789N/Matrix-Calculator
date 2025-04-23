dd4f8de41d6ad09ac593a0d949e1f6e6fd534277
Guilherme Aleixo de Oliveira Martins <20241144010020@ifrn.local>
2025-04-23 13:24:05 -0300

    limpada

--- python/trabaia.py
+++ python/trabaia.py
@@ -1,25 +1,5 @@
 from __future__ import annotations
 
-# transformar em classe?
-
-# 🤨
-# :)
-# (。﹏。*)
-
-
-#assim
-def somar(matrix1, matrix2):
-    assert(len(matrix1) == len(matrix2))
-
-    ret_matrix = [[0 for i in range(len(matrix1[0]))] for j in range(len(matrix1))]
-
-    for i in range(len(matrix1)):
-        for j in range(len(matrix2)):
-            ret_matrix[i][j] = matrix1[i][j] + matrix2[i][j]
-    
-    return ret_matrix
-
-# ou
 
 class matrix:
     def __init__(self: matrix, size: tuple[int, int], value: None|matrix = None):
@@ -60,8 +40,6 @@ class matrix:
         for i in range(self.size[0]):
             for j in range(other.size[1]):
                 for k in range(self.size[1]):
-                    #print(i, j, k, new_matrix.size, self.size, other.size)
-                    #print(other)
                     new_matrix.value[i][j] += self.value[i][k] * other.value[k][j]
         
         return new_matrix
@@ -75,9 +53,3 @@ class matrix:
             representation += "\n"
         
         return representation
-
-
-m1 = matrix((5, 2))
-m2 = matrix((2, 5))
-
-print(m1 * m2)
